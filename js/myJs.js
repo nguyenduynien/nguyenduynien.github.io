e0f53c7ba01851088b92a5aac93da885a0930c80
nguyenduynien <42843173+nguyenduynien@users.noreply.github.com>
2020-02-09 13:48:56 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -73,9 +73,20 @@ $('#no').click(() => {
 // generate text in input
 function textGenerate() {
     var n = "";
-    var text = "";
+    var text = " Vì Anh quá perfect ♥";
     var a = Array.from(text);
     var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
+    var count = textVal.length;
+    if (count > 0) {
+        for (let i = 1; i <= count; i++) {
+            n = n + a[i];
+            if (i == text.length + 1) {
+                $('#txtReason').val("");
+                n = "";
+                break;
+            }
+        }
+    }
     $('#txtReason').val(n);
     setTimeout("textGenerate()", 1);
 }
